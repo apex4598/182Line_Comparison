91242d313e9374095427cd7fe494c47bc02f45da
Apex4598 <login2ajit@gmail.com>
2022-08-06 07:13:00 +0530

    Added Functionality to Check Equality

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -21,9 +21,31 @@ public class LineComparison {
         double length1=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
         System.out.println("Length of line is :" +length1);
     }
+    public void checkEquality() {
+        System.out.println("Enter the value of x3 : ");
+        x3 = sc.nextInt();
+        System.out.println("Enter the value of y3 : ");
+        y3 = sc.nextInt();
+        System.out.println("Enter the value of x4: ");
+        x4 = sc.nextInt();
+        System.out.println("Enter the value of y4 : ");
+        y4 = sc.nextInt();
 
+        double length2 = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+
+        System.out.println("Length of line is :" + length2);
+        if (length1==length2) {
+            System.out.println("Two Lines are Equal");
+        } else {
+            System.out.println("Two Lines are Not Equal");
+        }
+    }
         public static void main (String[]args){
             System.out.println("Welcome to Line Comparison program ");
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.calculateLength();
+            lineComparison.checkEquality();
+
         }
     }
 
